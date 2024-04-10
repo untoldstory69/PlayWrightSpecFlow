e93716acd4171a08ae41bac8569e6a0c17fd2fd5
kishor <kishorsharma6918@gmail.com>
2024-04-10 15:19:22 +1000

    allure report generated

--- Features/TextBox.feature
+++ Features/TextBox.feature
@@ -3,16 +3,16 @@
 A short summary of the feature
 
 #@tag1
-#Scenario: TextBox Submit User Details
-#	Given I am on the TextBox page
-#	And I enter valid detais
-#	When I click Submit
-#	Then form should be submitted
+Scenario: TextBox Submit User Details
+	Given I am on the TextBox page
+	And I enter valid detais
+	When I click Submit
+	Then form should be submitted
 
-#Scenario:  a TextBox Submit User Details (failing this scennario intentionally for testing purpose)
-#	Given I am on the TextBox page
-#	And I enter valid detais
-#	When I click Submit
-	#Then form should be Not submitted
+Scenario:  a TextBox Submit User Details (failing this scennario intentionally for testing purpose)
+	Given I am on the TextBox page
+	And I enter valid detais
+	When I click Submit
+	Then form should be Not submitted
 	
 
--- Hooks/Hooks.cs
+++ Hooks/Hooks.cs
@@ -3,6 +3,7 @@ using Microsoft.Playwright;
 using NUnit.Framework;
 using PlayWrightSpecFlow.Drivers;
 using System.Reflection;
+using TechTalk.SpecFlow.Events;
 [assembly: Parallelizable(ParallelScope.Fixtures)]
 namespace PlayWrightSpecFlow.Hooks
 {
@@ -16,19 +17,28 @@ namespace PlayWrightSpecFlow.Hooks
 
         }
         [AfterStep]
-        public async Task AfterStep(ScenarioContext context) { 
-            if (context.TestError != null)
+        public async Task AfterStep(ScenarioContext context)
+        {
+            try
             {
-                var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-                var screenShotName = "failedTest.png";
-                  await _driver.Page.ScreenshotAsync(new PageScreenshotOptions
+
+                if (context.TestError != null)
+                {
+                    var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                    var screenShotName = "failedTest.png";
+                    await _driver.Page.ScreenshotAsync(new PageScreenshotOptions
                     {
                         Path = screenShotName
                     });
 
                     AllureApi.AddAttachment("TestScreenShot", "image/png", path + "\\" + screenShotName);
-                
+
+                }
             }
+            catch (Exception e) {
+                Console.WriteLine("An error occurred during cleanup",e.Message);
+            }
+
 
         }
        
