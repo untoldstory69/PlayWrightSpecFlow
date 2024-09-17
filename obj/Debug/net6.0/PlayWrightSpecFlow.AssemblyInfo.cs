fc103e076cac0e562b0103b1c1033c51b71538fb
kishor <kishorsharma6918@gmail.com>
2024-09-17 11:32:16 +1000

    gitflow action working and allure report generating successfully

--- obj/Debug/net6.0/PlayWrightSpecFlow.AssemblyInfo.cs
+++ obj/Debug/net6.0/PlayWrightSpecFlow.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayWrightSpecFlow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c65f8b70bf7f7a8ad64109470e1711fd86cf6646")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a337d95b16d2ee0464a9e3e51401ac937abf3f16")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayWrightSpecFlow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayWrightSpecFlow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
